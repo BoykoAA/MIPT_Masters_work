83586d716eb0ba198e363faa21bf77c86460c213
BoykoAA <BoykoA.A@yandex.ru>
2020-02-26 19:10:19 +0300

    add create_strings

--- scripts/get_sample.py
+++ scripts/get_sample.py
@@ -1,4 +1,4 @@
-def get_sample(matrix, sample_size=150, step=50):
+def get_sample(matrix, sample_size=550, step=200):
     sample = []
 
     beg_sample = 0
@@ -16,3 +16,14 @@ def get_sample(matrix, sample_size=150, step=50):
             end_sample = beg_sample + sample_size
 
     return sample
+
+
+def create_strings_for_dataset(samples_fft):
+    strings = []
+    for i in range(len(samples_fft)):
+        new_string = []
+        for n in range(samples_fft[i].shape[1]):
+            new_string.extend(samples_fft[i][:,n])
+        strings.append(new_string)
+
+    return strings
