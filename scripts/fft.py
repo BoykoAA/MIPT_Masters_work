adae49e104f03820a3c2fe011137750b0d903188
BoykoAA <BoykoA.A@yandex.ru>
2020-02-26 19:09:45 +0300

    up

--- scripts/fft.py
+++ scripts/fft.py
@@ -1,16 +1,15 @@
 import numpy as np
 
-def fft_for_sample(sample_calss):
-
+def fft_for_sample(sample_calss, first_n_elements=None):
     sample_class_fft = []
 
     for sample in sample_calss:
         new_sample = 0
         for idx, s in enumerate(range(sample.shape[0])):
             if idx == 0:
-                new_sample = np.fft.fft(sample[s])
+                new_sample = np.fft.fft(sample[s])[:first_n_elements]
             else:
-                new_sample = np.vstack((new_sample, np.fft.fft(sample[s])))
+                new_sample = np.vstack((new_sample, np.fft.fft(sample[s])[:first_n_elements]))
 
         sample_class_fft.append(new_sample)
 
